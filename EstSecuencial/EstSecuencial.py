68eaa7d7c0396886d4fe80499630e46b0b019c72
MarkPino <>
2021-09-28 17:01:12 +0000

    Estructuras

---
+++ EstSecuencial/EstSecuencial.py
@@ -0,0 +1,10 @@
+print("Hallar el area de un trapecio")
+#Datos de entrada
+BM=int(input("Ingrese la Base Mayor del trapecio: "))
+bm=int(input("Ingrese la Base Menor del trapecio: "))
+h=int(input("Ingrese la altura: "))
+#Proceso
+area=((BM*bm)*h)/2
+#Datos de salida
+print("El area del Trapecio es: ",area,"m2")
+calcpromfinal
