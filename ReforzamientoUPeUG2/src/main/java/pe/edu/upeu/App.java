276cc8385c961e26aa81106739b4b4305a49c836
Mark-Pino <mark.pino@upeu.edu.pe>
2021-10-24 20:34:18 +0000

    G2

---
+++ ReforzamientoUPeUG2/src/main/java/pe/edu/upeu/App.java
@@ -0,0 +1,38 @@
+package pe.edu.upeu;
+
+import java.util.Date;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void tiposDatos() {
+        //Variables
+        byte a=1;
+        short b=8;
+        int c=87;
+        float d=84.4f;
+        double f=87.21;
+        boolean e=true;
+        char h='+';
+        String g="hola";
+        Integer xx;
+        Double xxx;
+        Date ss;
+        
+    }
+
+    public static void operadores() {
+        
+    }
+
+    public static void controlStructura() {
+        
+    }
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
